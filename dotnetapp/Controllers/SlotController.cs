f10f17ea5f11996fec3804aae20fca47e0d9336f
Iamneo <techadmin@iamneo.ai>
2023-09-28 11:49:50 +0000

    Backup Commit

---
+++ dotnetapp/Controllers/SlotController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Exceptions; 
+using dotnetapp.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Controllers
+{
+    public class SlotController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SlotController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //write functionality here...
+            //return null;
+            return new Exception("Man Made Exception");
+        }
+
+        public IActionResult Create()
+        {
+            //return View();
+            return new Exception("Man Made Exception");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Slot slot)
+        {
+            //write functionality here with SlotBookingException "Slot capacity should not exceed 4."
+            //return null;        
+            return new Exception("Man Made Exception");
+        }
+    }
+}
